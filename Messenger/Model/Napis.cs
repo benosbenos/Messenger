ecd62de41d27c295692d051a492062076061611f
benos <mikiwieczorek2006@gmail.com>
2024-11-27 14:42:14 +0100

    koniec

---
+++ Messenger/Model/Napis.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Model
+{
+    public class Napis : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public string? Name { get; set; }
+        public string? ImageLink { get; set; }
+        
+        /*
+         *  Dla obiektu również potrzebujemy składnik odświeżania naszego widoku, jeżeli wiemy, że będzie również następowała zmiana wartości w naszej zmiennej
+         *  jak np. ilość książek
+         */
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Messenger/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Messenger.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+
+
+    namespace Messenger.ViewModel
+    {
+        public class MainPageViewModel : INotifyPropertyChanged
+        {
+            // Obiekt służący do tworzenia odświeżalnej listy
+            public ObservableCollection<Napis> Napisy { get; set; }
+
+            
+
+
+            public MainPageViewModel()
+            {
+                Napisy = new ObservableCollection<Napis>
+            {
+                new Napis { Id = 1, Name = "Jan Postulski", Description = "Kawa to moje życie", ImageLink = "osoba1.jpg"},
+                new Napis { Id = 2, Name = "Alicja Bystra", Description = "<brak opisu>", ImageLink = "osoba2.jpg" },
+                new Napis { Id = 3, Name = "Adrianna Nowak", Description = "Przyroda to ciężki przedmiot", ImageLink = "osoba3.jpg" },
+                new Napis { Id = 4, Name = "Grażyna Łuk", Description = "Życie mnie mnie", ImageLink = "osoba4.jpg"},
+                new Napis { Id = 5, Name = "Krystian Błąd", Description = "Life is fun", ImageLink = "osoba5.jpg"},
+                new Napis { Id = 6, Name = "Adrian Jańczyk", Description = "Droga do sukcesu nie jest prosta", ImageLink = "osoba6.jpg"},
+                new Napis { Id = 7, Name = "Michaś Kowalczyk", Description = "Gry to moja specjalność", ImageLink = "osoba7.jpg"},
+            };
+               
+            }
+           
+
+            /*
+             *  GŁÓWNE SKŁADNIKI NASZEJ STRUKTURY MVVM
+             *  Poza przypisaniem naszej klasy MainPageViewModel do interfejsu INotifyPropertyChanged potrzebujemy również
+             *  utworzyć zdarzenie PropertyChanged oraz funkcje OnPropertyChanged(string propertyName) które służy do
+             *  powiadomienia naszego widoku, że nastąpiła zmiana w naszych obiektach i ma odświeżyć dane w widoku!
+             */
+            public event PropertyChangedEventHandler? PropertyChanged;
+            public void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
